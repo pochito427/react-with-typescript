c40ec135c5147ba93580940a825f4f8fa878a619
Alfonso Jimenez <djalfo18@gmail.com>
2024-04-23 13:17:36 -0500

    Refactoring RandomFox component to receive typed props

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,9 +1,9 @@
-// generates a random number between 1 and 123
-const randomNumber = (): number => Math.floor(Math.random()*123)+1;
+type Props = {
+    image: string;
+}
 
-export const RandomFox = (): JSX.Element => {
-    const imageUrl = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+export function RandomFox({ image }: Props): JSX.Element{
     return (
-        <img width={320} height="auto" src={imageUrl} className="rounded" />
+        <img width={320} height="auto" src={image} className="rounded" />
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { RandomFox } from "@/components/RandomFox";
 
 export default function Home() {
+
+  // generates a random number between 1 and 122
+ const randomNumber = (): number => Math.floor(Math.random()*122)+1;
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${randomNumber()}.jpg`}/>
     </main>
   );
 }
