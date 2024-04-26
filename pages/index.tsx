5c92101c55cd3f7fdac8c95a1d0101f4a043f9c6
Alfonso Jimenez <djalfo18@gmail.com>
2024-04-26 12:27:00 -0500

    Implement useState hook to render an array of RandomFox images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
 export default function Home() {
@@ -5,12 +7,23 @@ export default function Home() {
   // generates a random number between 1 and 122
  const randomNumber = (): number => Math.floor(Math.random()*122)+1;
 
+ const [images, setImages] = useState<string[]>([
+  `https://randomfox.ca/images/${randomNumber()}.jpg`,
+  `https://randomfox.ca/images/${randomNumber()}.jpg`,
+  `https://randomfox.ca/images/${randomNumber()}.jpg`,
+  `https://randomfox.ca/images/${randomNumber()}.jpg`
+ ]);
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <RandomFox image={`https://randomfox.ca/images/${randomNumber()}.jpg`}/>
+      {images.map((image, index) => (
+        <div className="p-4" key={index}>
+          <RandomFox image={image} />
+        </div>
+      ))}
     </main>
   );
 }
