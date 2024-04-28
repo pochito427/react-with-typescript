d0c6d0d1a3e4c6df04d1995988d7212f44f01ace
Alfonso Jimenez <djalfo18@gmail.com>
2024-04-27 20:56:41 -0500

    Implementing new intersection observer with a typed ref node to update source image

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
 type Props = {
     image: string;
 }
 
 export function RandomFox({ image }: Props): JSX.Element{
+
+    const node = useRef<HTMLImageElement>(null);
+    const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+    useEffect(() => {
+        // new observer
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                   // intersecting
+                   setSrc(image);
+                }
+            });
+        });
+
+        // observe node
+        if(node.current){
+            observer.observe(node.current);
+        }
+
+        //disconnect
+        return () => {
+            observer.disconnect();
+        };
+
+    }, [image]);
+
+    
+
     return (
-        <img width={320} height="auto" src={image} className="rounded" />
+        <img ref={node} width={320} height="auto" src={src} className="mx-auto rounded-md bg-gray-300" />
     );
 }
